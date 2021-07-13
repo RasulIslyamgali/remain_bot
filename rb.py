a11ea5ec160cec30f7783bce71773c3b6d5f0f5f
rasul <rasul.islyamgali@gmail.com>
2021-07-13 19:02:13 +0300

    changing time to send daily message.

--- rb.py
+++ rb.py
@@ -76,7 +76,7 @@ def send_echo(message):
 
     def schedule_for_bot():
         # schedule.every(10).seconds.do(job)
-        schedule.every().day.at("20:29").do(job)
+        schedule.every().day.at("02:57").do(job)
         # schedule.every().day.at("00:22").do(job)
         schedule.cancel_job(job)
 
@@ -88,3 +88,7 @@ def send_echo(message):
 
 
 bot.polling(none_stop=True)
+
+
+
+
